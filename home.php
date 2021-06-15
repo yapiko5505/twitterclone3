a0918d57b96036f1584a8f1ca4f841a3cda7a5f0
yapiko5505 <84828867+yapiko5505@users.noreply.github.com>
2021-06-15 12:57:32 +0900

    Add files via upload

--- home.php
+++ home.php
@@ -1,28 +1,54 @@
-<?php
-// ホームコントローラー
 
-// 設定を読み込み
-include_once '../config.php';
-// 便利な関数を読み込み
-include_once '../util.php';
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    
+    <?php include_once('../views/common/head.php'); ?>
+    <title>ホーム画面/twitterクローン</title>
+    <meta name="discription" content="ホーム画面です">
 
-// ツイートデータ操作モデルを読み込む
-include_once '../Models/tweets.php';
+</head>
+<body class="home">
+    <div class="container">
+        <?php include_once('../views/common/side.php'); ?>
+        <div class="main">
+            <div class="main-header">
+                <h1>ホーム</h1>
+                
+            </div>
+            <div class="tweet-post">
+                <div class="my-icon">
+                    <img src="<?php echo htmlspecialchars($view_user['image_path']); ?>" alt="">
+                </div>
+                <div class="input-area">
+                    <form action="post.php" method="post" enctype="multipart/form-data">
+                        <textarea name="body" placeholder="いまどうしてる？" maxlength="140"></textarea>
+                        <div class="bottom-area">
+                            <div class="mb-0">
+                                <input type="file" name="image" class="form-control form-control-sm">
+                            </div>
+                            <button class="btn" type="submit">つぶやく</button>
+                        </div>
+                    </form>
 
-// ログインしているか
-$user = getUserSession();
-if (!$user) {
-    // ログインしていない
-    header('Location:' . HOME_URL .'Controllers/sign-in.php');
-    exit;
-}
+                </div>
+            </div>
+            <div class="ditch"></div>
 
-// 画面表示
-$view_user = $user;
+            <?php if (empty($view_tweets)): ?>
+                <p class="p-3">ツイートがまだありません</p>
+            <?php else: ?>
+                <div class="tweet-list">
+                    <?php foreach ($view_tweets as $view_tweet): ?>
+                        <?php include('../views/common/tweet.php'); ?>
+                    <?php endforeach; ?>
 
-// ツイート一覧作成
-// TODO:あとでDBから取得
-$view_tweets = findTweets($user); 
+                </div>
+            <?php endif;?>
+        </div>
+    </div>
 
+    <?php include_once('../views/common/foot.php'); ?>
+</body>
 
-include_once '../views/home.php';
+</html>
